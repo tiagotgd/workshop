d750c447b42db9852479d43a220871f973e63be1
Tiago Garcia Dias <tiago.dias@ottobock.com.br>
2015-09-11 15:22:45 -0300

    incluido var self

--- js/main.js
+++ js/main.js
@@ -22,8 +22,9 @@ var app = {
     },
     
     initialize: function() {
-        this.store = new MemoryStore(function() {
-            self.showAlert('Store Initialized', 'Info');
+    	var self = this;
+    	this.store = new MemoryStore(function() {
+        self.showAlert('Store Initialized', 'Info');
         });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
